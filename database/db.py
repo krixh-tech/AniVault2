22bda856957f280eb97c86e7f99c9583d1d9c9e6
krixh-tech <idf59161@gmail.com>
2026-04-06 11:40:10 +0530

    Initialize MongoDB connection in db.py

---
+++ database/db.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["anime_bot"]
+
+def get_db():
+    return db
