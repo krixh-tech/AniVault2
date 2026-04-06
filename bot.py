fbb22f2fe5229c499eb04cffb48a8e8cb0bdc607
krixh-tech <idf59161@gmail.com>
2026-04-06 11:44:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,20 @@
+from telegram.ext import Application, CommandHandler
+from handlers.start import start_cmd, help_cmd
+import os
 
-from handlers.start import start_cmd
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 def main():
-    print("Bot structure OK. Replace with full bot code.")
+
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    # commands
+    app.add_handler(CommandHandler("start", start_cmd))
+    app.add_handler(CommandHandler("help", help_cmd))
+
+    print("Bot Started Successfully")
+
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
